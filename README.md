a77b6f6fb4957f2cb40c370f76b5a7de177a5a66
Richard Barkestam <richard.barkestam@syncron.com>
2019-10-16 21:18:07 +0200

    Clean up

--- README.md
+++ README.md
@@ -1,16 +1,2 @@
 # SMA Modbus
 Homey app for monitoring SMA inverters through modbus protocol interface.
-
-## Updates
-- New device type, Tripower
-- New information settings, showing properties from inverter such as device type, serial number, software version and max capacity.
-- New decoder functions that honour the SMA data type specs
-- Additional device properties; DC voltage for Tripower dual MPP trackers, A and B. Condition and operational status.
-- Moved SMA modbus logic to separate file(s) enabling easy testing outside Homey
-- If settings are updated the new values are used immediately, no app restart required
-- Grid power is stored in insights
-
-
-TODO
-Homey version 3.0
-- Change device class to solarpanel
--- drivers/inverter/device.js
+++ drivers/inverter/device.js
@@ -3,7 +3,6 @@
 const Homey = require('homey');
 const modbus = require('jsmodbus');
 const net = require('net');
-const decodeData = require('../../lib/decodeData.js');
 const socket = new net.Socket();
 
 class SmaModbusDevice extends Homey.Device {
@@ -39,35 +38,40 @@ class SmaModbusDevice extends Homey.Device {
 
       this.pollingInterval = setInterval(() => {
         Promise.all([
-          client.readHoldingRegisters(30775, 2), //S32 AC Power
-          client.readHoldingRegisters(30535, 2), //U32 Daily Yield Wh
-          client.readHoldingRegisters(30783, 2), //U32, 2 decimals, Grid voltage phase L1
-          client.readHoldingRegisters(30529, 2), //U32, Total yield Wh
+          client.readHoldingRegisters(30775, 2),
+          client.readHoldingRegisters(30517, 4),
+          client.readHoldingRegisters(30783, 2),
+          client.readHoldingRegisters(30531, 2)
         ]).then((results) => {
-          let powerac = decodeData.decodeS32(results[0].response._body._valuesAsArray, 0, 0)
-          let daily_yield = decodeData.formatWHasKWH(decodeData.decodeU32(results[1].response._body._valuesAsArray, 0, 0));
-          let voltage = decodeData.decodeU32(results[2].response._body._valuesAsArray, 2, 0)
-          let total_yield = decodeData.formatWHasMWH(decodeData.decodeU32(results[3].response._body._valuesAsArray, 0, 0));
+          var powerac = results[0].response._body._valuesAsArray[1];
+          var daily_yield = results[1].response._body._valuesAsArray[3];
+          var voltage = results[2].response._body._valuesAsArray[1];
+          var total_yield = results[3].response._body._valuesAsArray[1];
 
           // POWER
           if (this.getCapabilityValue('measure_power') != powerac) {
-            this.setCapabilityValue('measure_power', powerac);
+            if (powerac < 0 || powerac > 10000) {
+              this.setCapabilityValue('measure_power', 0);
+            } else {
+              this.setCapabilityValue('measure_power', powerac);
+            }
           }
 
           /* DAILY YIELD */
-          if (this.getCapabilityValue('meter_power') != daily_yield) {
-            this.setCapabilityValue('meter_power', daily_yield);
-          }
+          var meterpower = daily_yield / 1000;
+          this.setCapabilityValue('meter_power', meterpower);
 
           /* VOLTAGE */
-          if (this.getCapabilityValue('measure_voltage') != voltage) {
-            this.setCapabilityValue('measure_voltage', voltage);
+          if (voltage === 65535) {
+            this.setCapabilityValue('measure_voltage', 0);
+          } else {
+            var volt = voltage / 100;
+            this.setCapabilityValue('measure_voltage', volt);
           }
 
           /* TOTAL YIELD */
-          if (this.getCapabilityValue('measure_yield') != total_yield) {
-            this.setCapabilityValue('measure_yield', total_yield);
-          }
+          var measureyield = total_yield / 1000;
+          this.setCapabilityValue('measure_yield', measureyield);
         }).catch((err) => {
           this.log(err);
         })
