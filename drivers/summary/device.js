6ffe3a10ff9b3966ec86d3edaad03bca62ea5a60
Richard Barkestam <richard.barkestam@syncron.com>
2020-03-09 21:04:10 +0100

    Added  MPP capabilities to summary device

--- drivers/summary/device.js
+++ drivers/summary/device.js
@@ -11,31 +11,90 @@ class SummaryDevice extends Homey.Device {
         this.pollIntervals = [];
         this.summary = {
             name: this.getName(),
-            polling: this.getSettings().polling
+            polling: this.getSettings().polling,
+            showMPP: this.getSettings().show_mpp,
+            invertersMPPConfig: null
         };
 
+        this.summary.invertersMPPConfig = this.getInverterMPPConfig();
+        this.setupCapabilities();
         this._initilializeTimers();
+    }
+
+    setupCapabilities() {
+        this.log('Setting up capabilities');
+        if (this.summary.showMPP === 'yes') {
+            if (this.summary.invertersMPPConfig.MPP_A) {
+                this.showCapability('power_pv.dcA', this.summary.invertersMPPConfig.MPP_A_LBL);
+            }
+            if (this.summary.invertersMPPConfig.MPP_B) {
+                this.showCapability('power_pv.dcB', this.summary.invertersMPPConfig.MPP_B_LBL);
+            }
+        } else {
+            this.hideCapability('power_pv.dcA');
+            this.hideCapability('power_pv.dcB');
+        }
+    }
+
+    showCapability(capabilityName, label) {
+        //Device should have capability
+        if (!this.hasCapability(capabilityName)) {
+            this.log(`Adding missing capability '${capabilityName}' with label '${label}'`);
+            this.addCapability(capabilityName);
+            this.setCapabilityOptions(capabilityName, {title: { en: label}});
+        } else {
+            this.log(`Device has capability '${capabilityName}'`);
+        }
+    }
+
+    hideCapability(capabilityName) {
+        //Device doesnt have capability, remove it
+        this.log(`Removing capability '${capabilityName}'`);
+        this.removeCapability(capabilityName);
+    }
+
+    getInverterMPPConfig() {
+        let mpp = {MPP_A: true, MPP_A_LBL: '' ,MPP_B: true, MPP_B_LBL: ''};
+        ManagerDrivers.getDriver('inverter').getDevices().forEach(function (inverter) {
+            if (!inverter.hasCapability('measure_power.dcA')) {
+                mpp.MPP_A = false;
+            } else {
+                mpp.MPP_A_LBL = inverter.getCapabilityOptions('measure_power.dcA').title.en;
+            }
+            if (!inverter.hasCapability('measure_power.dcB')) {
+                mpp.MPP_B = false;
+            } else {
+                mpp.MPP_B_LBL = inverter.getCapabilityOptions('measure_power.dcB').title.en;
+            }
+        });
+        return mpp;
     }
 
     updateValues() {
 
         let battery_charge = 0;
         let battery_discharge = 0;
-        ManagerDrivers.getDriver('storage').getDevices().forEach(function(inverter) {
+        ManagerDrivers.getDriver('storage').getDevices().forEach(function (inverter) {
             battery_charge = battery_charge + inverter.getCapabilityValue('measure_power.charge');
             battery_discharge = battery_discharge + inverter.getCapabilityValue('measure_power.discharge')
         });
         this._updateProperty('power_drawn.battery', (battery_charge - battery_discharge));
 
         let power_pv = 0;
-        ManagerDrivers.getDriver('inverter').getDevices().forEach(function(inverter) {
+        let power_MPPA = 0;
+        let power_MPPB = 0;
+        ManagerDrivers.getDriver('inverter').getDevices().forEach(function (inverter) {
             power_pv = power_pv + inverter.getCapabilityValue('measure_power');
+            power_MPPA = power_MPPA + inverter.getCapabilityValue('measure_power.dcA') || 0;
+            power_MPPB = power_MPPB + inverter.getCapabilityValue('measure_power.dcB') || 0;
         });
         this._updateProperty('power_pv', power_pv);
+        this._updateProperty('power_pv.dcA', power_MPPA);
+        this._updateProperty('power_pv.dcB', power_MPPB);
 
         let power_grid = 0;
         let surplus = 0;
-        ManagerDrivers.getDriver('energy').getDevices().forEach(function(em) {
+        ManagerDrivers.getDriver('energy').getDevices().forEach(function (em) {
             power_grid = power_grid + em.getCapabilityValue('measure_power');
             surplus = surplus + em.getCapabilityValue('measure_power.surplus');
         });
@@ -88,18 +147,20 @@ class SummaryDevice extends Homey.Device {
     }
 
     async onSettings(oldSettings, newSettings, changedKeysArr) {
-        let change = false;
+
         if (changedKeysArr.indexOf("polling") > -1) {
             this.log('Polling value was change to:', newSettings.polling);
             this.summary.polling = newSettings.polling;
-            change = true;
-        }
-
-        if (change) {
             this._reinitializeTimers();
         }
-    }
 
+        if (changedKeysArr.indexOf("show_mpp") > -1) {
+            this.log('Show MPP value was change to:', newSettings.show_mpp);
+            this.summary.showMPP = newSettings.show_mpp;
+            this.summary.invertersMPPConfig = this.getInverterMPPConfig();
+            this.setupCapabilities();
+        }
+    }
 }
 
 module.exports = SummaryDevice;
