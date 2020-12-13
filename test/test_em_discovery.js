ccf3f19b5e41cc25a72efc0215c40b4fe828653e
Richard Barkestam <richard.barkestam@syncron.com>
2020-12-13 21:47:55 +0100

    Packages and EnergyMeter test

---
+++ test/test_em_discovery.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const EnergyMeter = require('../lib/sma_em.js');
+
+let devices = [];
+let emSession = new EnergyMeter({});
+
+emSession.on('readings', readings => {
+    if (!devices.find((em) => em.data.id === readings.serialNo)) {
+        console.log(`Adding device: ${readings.serialNo}`);
+        devices.push({
+            name: `Energy Meter (${readings.serialNo})`,
+            data: {
+                id: readings.serialNo
+            }
+        });
+    }
+});
+
+//Wait for some time and see what we find
+sleep(5000).then(() => {
+    try {
+        emSession.disconnect();
+        console.log(devices);
+    } catch (err) {
+        console.log(err);
+    }
+}).catch(reason => {
+    console.log('Timeout error', reason);
+});
+
+// sleep time expects milliseconds
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
