279740e9a156e87841db0f41e4ecea2acd9794f2
Richard Barkestam <richard@barkestam.se>
2025-02-13 09:31:21 +0100

    Add global fetch and AbortSignal polyfills for node-fetch compatibility

--- app.js
+++ app.js
@@ -7,7 +7,23 @@ class SmaModbusApp extends App {
 
   async onInit() {
     this.homeyLog = new Log({ homey: this.homey });
+    this.setupGlobalFetch();
     this.log('Initializing SMA Modbus app ...');
+  }
+
+  setupGlobalFetch() {
+    if (!global.fetch) {
+      global.fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+    }
+    if (!global.AbortSignal.timeout) {
+      global.AbortSignal.timeout = timeout => {
+        const controller = new AbortController();
+        const abort = setTimeout(() => {
+          controller.abort();
+        }, timeout);
+        return controller.signal;
+      }
+    }
   }
 }
 
