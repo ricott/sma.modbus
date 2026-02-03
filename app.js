5455e1b783ef2ccd6746ab5730154d926e18811d
Richard Barkestam <richard@barkestam.se>
2026-02-03 08:31:06 +0100

    Update version to 2.8.2; add maintenance entry in changelog, remove unused dependencies, and add .homeyignore file

--- app.js
+++ app.js
@@ -10,23 +10,7 @@ class SmaModbusApp extends App {
     if (process.env.DEBUG == '1') {
       require('inspector').open(9222, '0.0.0.0', true);
     }
-    this.setupGlobalFetch();
     this.log('Initializing SMA Modbus app ...');
-  }
-
-  setupGlobalFetch() {
-    if (!global.fetch) {
-      global.fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-    }
-    if (!global.AbortSignal.timeout) {
-      global.AbortSignal.timeout = timeout => {
-        const controller = new AbortController();
-        const abort = setTimeout(() => {
-          controller.abort();
-        }, timeout);
-        return controller.signal;
-      }
-    }
   }
 }
 
